d3573a255110fd57eb5db18bb5f8abac0faf0907
Pranav <pranav279.v@gmail.com>
2024-12-29 21:37:37 +0530

    init

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+    private static void sendCommand(String host, int port, String command) {
+        try (Socket socket = new Socket(host, port);
+             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            System.out.println("Connected to server at " + host + ":" + port);
+
+            // Send the command
+            System.out.println("Sending command: " + command);
+            writer.print(command);
+            writer.flush();
+
+            // Read the response
+            String response = reader.readLine();
+            System.out.println("Server response: " + response);
+
+        } catch (IOException e) {
+            System.err.println("Error communicating with server: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        // Define the server address and port
+        String serverHost = "localhost";
+        int serverPort = 6379;
+
+        // Example RESP commands to test
+        String bulkCommand = "$5\r\nHello\r\n";
+        String arrayCommand = "*2\r\n$5\r\nHello\r\n$5\r\nWorld\r\n";
+
+        // Send commands to the server
+        sendCommand(serverHost,serverPort,bulkCommand);
+        sendCommand(serverHost,serverPort,arrayCommand);
+    }
+}
+
--- src/main/java/org/example/Resp.java
+++ src/main/java/org/example/Resp.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Resp {
 
     final static char STR = '+';
@@ -8,8 +15,89 @@ public class Resp {
     final static char BULK = '$';
     final static char ARR = '*';
 
-    private String type;
-    private String str;
+    static class Value {
+        char type;
+        String str;
+        int num;
+        String bulk;
+        List<Value> array;
 
+        Value() {
+            this.array = new ArrayList<>();
+        }
+    }
+
+    private BufferedReader reader;
+    public Resp(InputStream inputStream) {
+        this.reader = new BufferedReader(new InputStreamReader(inputStream));
+    }
+
+    private String readLine() throws IOException {
+        String line = reader.readLine();
+        if (line == null)
+            throw new IOException("End of stream!!");
+        return line;
+    }
+
+    private Value readInt() throws IOException {
+        Value value = new Value();
+        value.type = INT;
+        value.num = readInteger();
+        return value;
+    }
+
+    private Value readString() throws IOException {
+        Value value = new Value();
+        value.type = STR;
+        value.str = readLine();
+    }
+
+    private int readInteger() throws IOException {
+        String line =readLine();
+        return Integer.parseInt(line);
+    }
+
+    private Value readArray() throws IOException {
+        Value value = new Value();
+        value.type = ARR;
+        int length = readInteger();
+        for (int i=0;i<length;i++)
+            value.array.add(read());
+        return value;
+    }
+
+    private Value readBulk() throws IOException {
+        Value value = new Value();
+        value.type = BULK;
+        int length = readInteger();
+        if (length == -1) {
+            value.bulk = null;
+            return value;
+        }
+
+        char[] bulkData = new char[length];
+        int byteRead = reader.read(bulkData);
+        if (byteRead != length)
+            throw new IOException("Incomplete bulk string data!!");
+
+        value.bulk = new String(bulkData);
+        //go to end of line for line.
+        readLine();
+        return value;
+    }
+
+    public Value read() throws IOException {
+        int type = reader.read();
+        if (type == -1)
+            throw new IOException("End of stream.");
+        switch (type) {
+            case ARR:   return readArray();
+            case BULK:  return readBulk();
+            case INT:   return readInt();
+            case STR:   return readString();
+            case ERR:   throw new IOException("Unknown Exception of Datatype!!");
+            default:    return
+        }
+    }
 
 }
